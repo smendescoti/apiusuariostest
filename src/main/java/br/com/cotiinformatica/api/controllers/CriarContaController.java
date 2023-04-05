3891345189fdf127f41312a1408635b34a9f4814
smendescoti <sergio.coti@gmail.com>
2023-04-05 00:47:16 -0300

    first commit

--- src/main/java/br/com/cotiinformatica/api/controllers/CriarContaController.java
+++ src/main/java/br/com/cotiinformatica/api/controllers/CriarContaController.java
@@ -3,6 +3,7 @@ package br.com.cotiinformatica.api.controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -23,5 +24,10 @@ public class CriarContaController {
 
 		CriarContaResponseDTO response = usuarioAppService.criarConta(dto);
 		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
+	
+	@GetMapping("/api/usuarios/consulta")
+	public String get() {
+		return "Teste";
 	}
 }
